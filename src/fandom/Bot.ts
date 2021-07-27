455cfc1c027496fba5e23cfb8ae86cd3bfdc8228
bitomic <35199700+bitomic@users.noreply.github.com>
2021-07-26 20:33:26 -0600

    feat: regenerate csrf token when an edit fails due to a badtoken or notoken error

--- src/fandom/Bot.ts
+++ src/fandom/Bot.ts
@@ -10,6 +10,8 @@ export class Bot {
 	readonly #username: string
 	readonly #wiki: Wiki
 
+	#csrf?: string
+
 	constructor( {
 		password, username, wiki
 	}: { password: string, username: string, wiki: Wiki } ) {
@@ -19,13 +21,31 @@ export class Bot {
 	}
 
 	async edit( params: IApiEditRequest ): Promise<IEditResponse> {
-		const token = await this.#wiki.getToken( 'csrf' )
+		const token = await this.getCSRFToken()
 		return this.#wiki.post<IEditResponse>( {
 			...params,
 			action: 'edit',
 			assert: 'user',
-			token: token.query.tokens.csrftoken
+			token
 		} )
+			.catch( async ( e: ApiError ) => {
+				Logger.warn( 'There was an error with the action. Regenerating CSRF and trying again...' )
+				if ( e.code === 'notoken' || e.code === 'badtoken' ) {
+					await this.getCSRFToken( true )
+					return this.edit( params )
+				}
+
+				throw e
+			} )
+	}
+
+	async getCSRFToken( force = false ): Promise<string> {
+		if ( force ) {
+		//if ( force || !this.#csrf ) {
+			const token = await this.#wiki.getToken( 'csrf' )
+			this.#csrf = token.query.tokens.csrftoken
+		}
+		return this.#csrf as string
 	}
 
 	async login(): Promise<ILoginResponse> {
