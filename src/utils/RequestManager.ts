9a3b0f87e85469b811e98771276063b6ff822cfc
bitomic <35199700+bitomic@users.noreply.github.com>
2021-07-28 20:48:23 -0600

    refactor: use node-fetch instead of request for request manager

--- src/utils/RequestManager.ts
+++ src/utils/RequestManager.ts
@@ -1,64 +1,60 @@
-import {
-	Response as RequestResponse,
-	defaults as request
-} from 'request'
+import fetch, { Response } from 'node-fetch'
+import FormData from 'form-data'
 import fs from 'fs'
+import tough from 'tough-cookie'
 
 export class RequestManager {
-	private ctx = request( {
-		jar: true
-	} )
+	#jar = new tough.CookieJar()
 
-	get<T>( {
+	async get<T>( {
 		url, qs
 	}: { url: string, qs: Record<string, string> } ): Promise<T> {
-		return new Promise( resolve => {
-			this.ctx.get(
-				{
-					qs,
-					url
-				},
-				( _error, _res, body ) => {
-					resolve( JSON.parse( body ) )
-				}
-			)
+		const params = new URLSearchParams( qs )
+		const req = await fetch( `${url}?${params}`, {
+			headers: {
+				cookie: this.#jar.getCookieStringSync( url )
+			},
+			method: 'GET'
 		} )
+
+		const cookies = req.headers.raw()[ 'set-cookie' ] || []
+		for ( const cookie of cookies ) {
+			this.#jar.setCookieSync( cookie, url )
+		}
+
+		return req.json()
 	}
 
-	post<T>( {
+	async post<T>( {
 		url, form
 	}: { url: string, form: Record<string, string | fs.ReadStream> } ): Promise<T> {
-		return new Promise( resolve => {
-			const findReadStream = Object.values( form )
-				.find( i => i instanceof fs.ReadStream )
-			const hasReadStream = findReadStream !== undefined
+		const formData = new FormData()
+		for ( const prop in form ) {
+			formData.append( prop, form[ prop ] )
+		}
 
-			const key = hasReadStream ? 'formData' : 'form'
-
-			const params = {
-				[ key ]: form,
-				url
-			}
-
-			this.ctx.post(
-				params,
-				( _error, _res, body ) => {
-					resolve( JSON.parse( body ) )
-				}
-			)
+		const req = await fetch( url, {
+			body: formData,
+			headers: {
+				cookie: this.#jar.getCookieStringSync( url )
+			},
+			method: 'POST'
 		} )
+
+		const cookies = req.headers.raw()[ 'set-cookie' ] || []
+		for ( const cookie of cookies ) {
+			this.#jar.setCookieSync( cookie, url )
+		}
+
+		return req.json()
 	}
 
-	raw( url: string ): Promise<RequestResponse> {
-		return new Promise( resolve => {
-			this.ctx.get(
-				{
-					url
-				},
-				( _error, res ) => {
-					resolve( res )
-				}
-			)
+	raw( url: string ): Promise<Response> {
+		return fetch( url, {
+			headers: {
+				cookie: this.#jar.getCookieStringSync( url )
+			},
+			method: 'GET'
 		} )
 	}
 }
