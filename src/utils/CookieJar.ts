5a069e2a45072b1ab01f30ba6d92ae21c5a0c3c5
bitomic <35199700+bitomic@users.noreply.github.com>
2021-09-17 21:36:44 -0600

    feat(utils): custom cookie jar wrapper

---
+++ src/utils/CookieJar.ts
@@ -0,0 +1,96 @@
+import fs from 'fs-extra'
+import tough from 'tough-cookie'
+
+declare module 'tough-cookie' {
+	interface CookieJar {
+		store: tough.Store
+	}
+}
+
+export interface ICookieStoreOptions {
+	regex: RegExp | RegExp[]
+	path: string
+}
+
+export interface ICookieStorage {
+	[ url: string ]: tough.Cookie[]
+}
+
+export class CookieJar {
+	#storage: ICookieStorage
+	#store?: ICookieStoreOptions
+	#prettify: boolean
+
+	constructor( {
+		prettify, store
+	}: { prettify?: boolean, store?: ICookieStoreOptions } = {
+	} ) {
+		this.#prettify = prettify ?? false
+		this.#storage = {
+		}
+		this.#store = store
+
+		if ( !this.#store || !fs.existsSync( this.#store.path ) ) return
+		this.#storage = fs.readJsonSync( this.#store.path )
+
+		for ( const host in this.#storage ) {
+			this.#storage[ host ] = this.#storage[ host ]
+				.map( cookie => tough.Cookie.fromJSON( cookie ) )
+				.filter( ( cookie ): cookie is tough.Cookie => cookie !== null )
+				.filter( cookie => Date.now() < cookie.expiryTime() )
+		}
+	}
+
+	clear( url: string ): void {
+		const {
+			host
+		} = new URL( url )
+		if ( !this.#storage[ host ] ) return
+		delete this.#storage[ host ]
+	}
+
+	clearAll(): void {
+		this.#storage = {
+		}
+	}
+
+	get( url: string ): string {
+		const {
+			host
+		} = new URL( url )
+		if ( !this.#storage[ host ] ) return ''
+		return this.#storage[ host ].join( ';' )
+	}
+
+	set( {
+		cookie, url
+	}: { cookie: string, url: string } ): void {
+		const toughcookie = tough.parse( cookie )
+
+		if ( !toughcookie || !this.allowCookie( toughcookie ) ) return
+
+		const {
+			host
+		} = new URL( url )
+		if ( !this.#storage[ host ] ) this.#storage[ host ] = []
+		this.#storage[ host ].push( toughcookie )
+
+		this.save()
+	}
+
+	private allowCookie( cookie: tough.Cookie ): boolean {
+		if ( !this.#store?.regex ) return true
+		const regexes = Array.isArray( this.#store.regex ) ? this.#store.regex : [ this.#store.regex ]
+		const match = regexes.find( regex => cookie.key.match( regex ) )
+		return match !== undefined
+	}
+
+	private save(): void {
+		if ( !this.#store ) return
+
+		fs.ensureFileSync( this.#store.path )
+		fs.writeJSONSync( this.#store.path, this.#storage, {
+			spaces: this.#prettify ? '\t' : undefined
+		} )
+	}
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+export * from './CookieJar'
 export * from './Logger'
 export * from './RequestManager'
 export * from './sleep'
