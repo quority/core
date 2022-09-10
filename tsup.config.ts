7e7fad4e91feaec719f301cd3c08ac4373e67896
bitomic <35199700+bitomic@users.noreply.github.com>
2022-09-10 11:40:35 -0600

    chore: generate declaration files

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig( {
 	clean: true,
+	dts: true,
 	entry: [ 'src/main.ts' ],
 	format: [ 'cjs', 'esm' ],
 	minify: true,
 	sourcemap: true,
 	splitting: true,
-	treeshake: true
+	treeshake: true,
 } )
